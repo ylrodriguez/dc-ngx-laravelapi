a02aaa6d7b3faab73ae59280ac0149348d4ebd8e
Yojhan <yojhanr95@gmail.com>
2019-09-09 21:23:44 -0500

    Edit styles Login Component (Frontend)

--- DeepCartWebApp/src/app/app.module.ts
+++ DeepCartWebApp/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/accounts/login/login.component';
 import { SignupComponent } from './components/accounts/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
@@ -13,11 +15,13 @@ import { SignupComponent } from './components/accounts/signup/signup.component';
     AppComponent,
     NavbarComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- DeepCartWebApp/src/app/components/accounts/login/login.component.ts
+++ DeepCartWebApp/src/app/components/accounts/login/login.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  
+  private form: Object = {
+    email: '',
+    password: ''
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    console.log("Recibo esto:")
+    console.log(this.form)
   }
 
 }
