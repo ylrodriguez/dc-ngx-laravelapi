fe910cf1cd510d32d4b82599ffb4085cbeccf10b
Yojhan <ylrodriguez024@gmail.com>
2019-09-15 17:21:23 -0500

    Add offerDiscount to ProductCard Component

--- DeepCartWebApp/src/app/components/product/product-card/product-card.component.ts
+++ DeepCartWebApp/src/app/components/product/product-card/product-card.component.ts
@@ -10,10 +10,16 @@ export class ProductCardComponent implements OnInit {
   @Input() imgUrl: string;
   @Input() price: string;
   @Input() name: string;
+  @Input() offerDiscount: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getDiscountPrice(price, offerDiscount){
+    let discount = price * (offerDiscount/100);
+    return price - discount;
   }
 
 }
