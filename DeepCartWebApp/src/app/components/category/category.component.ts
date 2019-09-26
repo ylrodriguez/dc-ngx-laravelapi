bc2e850c9fc300beca3a05d8be6d04f92fa04719
Yojhan <yojhanr95@gmail.com>
2019-09-26 10:04:41 -0500

    Add Order By button to Category component. It allows to sort by price and id

--- DeepCartWebApp/src/app/components/category/category.component.ts
+++ DeepCartWebApp/src/app/components/category/category.component.ts
@@ -15,8 +15,11 @@ export class CategoryComponent implements OnInit, OnDestroy {
   private routeSub: any;
   private currentCategory: Category;
   private categoryProducts: Product[];
+  private sortedProducts: Product[];
   brandList: string[] = [];
   slug:string;
+  isSorting: boolean = true;
+  sortAction: string = "default";
 
   constructor(
     private route: ActivatedRoute,
@@ -51,12 +54,51 @@ export class CategoryComponent implements OnInit, OnDestroy {
           this.brandList.indexOf(p.brand) === -1 ? this.brandList.push(p.brand) : console.log("")
         }
 
+        this.sortedProducts = JSON.parse(JSON.stringify(this.categoryProducts))
+        this.isSorting = false;
+
       },
       (err) => {
         console.log(err)
       }
     )
   }
+
+  sortBy(e, action){
+    e.preventDefault();
+
+    if(action == 'pricelowest'){
+      this.sortedProducts.sort( this.dynamicSort("price"))
+      this.sortAction = action;
+    }
+    if(action == 'pricehighest'){
+      this.sortedProducts.sort( this.dynamicSort("-price"))
+      this.sortAction = action;
+    }
+    if(action == 'default'){
+      this.sortedProducts.sort( this.dynamicSort("id"))
+      this.sortAction = action;
+    }
+
+  }
+
+  dynamicSort(property) {
+    /*
+      la función array.sort recibe un callback que implementa (a, b) para comparar los elementos
+      si (a, b) retorna -1 a viene primero que b. Se posiciona en un indice menor
+      si (a, b) retorna 0 son iguales y no se alteran, pero se posicionan respectivamente respecto a los demas
+      si (a, b) retorna 1 b viene primero que a. Se posiciona en un indice menor
+    */
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a, b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
 
 
   ngOnDestroy(){
