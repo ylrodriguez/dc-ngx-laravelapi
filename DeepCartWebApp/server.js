717c4973c3f1b6a4720f9eb5838c72eca3783ef0
Yojhan <yojhanr95@gmail.com>
2020-05-15 19:08:25 -0500

    Update app to use Heroku, editing package.json and creating an express server in server.js

--- DeepCartWebApp/server.js
+++ DeepCartWebApp/server.js
@@ -4,12 +4,13 @@ const path = require('path');
 
 const app = express();
 
-// Replace the '/dist/<to_your_project_name>'
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/DeepCartWebApp'));
 
-app.get('*', function (req, res) {
-    // Replace the '/dist/<to_your_project_name>/index.html'
-    res.sendFile(path.join(__dirname + '/dist/DeepCartWebApp/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/DeepCartWebApp/index.html'));
 });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
--- README.md
+++
@@ -1,7 +0,0 @@
-# DeepCart-ngEcommerce
-Laravel 5.6 Backend - API  
-Angular 8 WebApp
-
-
-* Deploy Laravel Project using Elastic Beanstalk: https://www.youtube.com/watch?v=ISVaMijczKc&t=760s   
-* Deploy Angular Project using Amazon S3 Buckets: https://www.youtube.com/watch?v=RUKcrphvO8I  
